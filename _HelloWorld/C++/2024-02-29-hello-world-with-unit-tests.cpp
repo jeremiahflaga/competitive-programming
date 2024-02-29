b5223d83074c79f5f08a19ba4d2f1b8daba2b174
jflaga <flaga.jeremiah@gmail.com>
2024-03-01 00:03:05 +0800

    Added main() template in C++ hello world file

--- _HelloWorld/C++/2024-02-29-hello-world-with-unit-tests.cpp
+++ _HelloWorld/C++/2024-02-29-hello-world-with-unit-tests.cpp
@@ -21,3 +21,20 @@ TEST_CASE("testing hello") {
     
     CHECK(hello("Jürgen") == "Hello, Jürgen!");
 }
+
+
+// int main() { 
+//     // Redirect input/output
+//     // freopen("input.txt", "r", stdin);
+//     // freopen("output.txt", "w", stdout);
+
+//     // body 
+// }
+
+// int main(int argc, char* argv[]) { 
+//     // Redirect input/output
+//     // freopen("input.txt", "r", stdin);
+//     // freopen("output.txt", "w", stdout);
+
+//     // body 
+// }
