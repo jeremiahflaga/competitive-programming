ce1a1779f60a6dc5859ad05ed3bf3a18ab356e70
jflaga <flaga.jeremiah@gmail.com>
2024-03-01 01:12:11 +0800

    CtCI VII count integer pairs with difference k, using hash table

--- cracking-the-coding-interview-6e/00-VII-technical-questions/01.1-integer-pairs-with-difference-k/2024-02-29-integer-pairs-with-difference-k.cpp
+++ cracking-the-coding-interview-6e/00-VII-technical-questions/01.1-integer-pairs-with-difference-k/2024-02-29-integer-pairs-with-difference-k.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <unordered_map>
 
 using namespace std;
 
@@ -26,8 +27,23 @@ int countIntegerPairsWithDifferenceK_BruteForce(vector<int> v, int k) {
     return count;
 }
 
+int countIntegerPairsWithDifferenceK_UsingHashTable(vector<int> v, int k) {
+    unordered_map<int, int> hashTable;
+    for (int x : v) {
+        hashTable[x] = x;
+    }
+    int count = 0;
+    for (int i = 0; i < v.size(); i++) {
+        if(hashTable.find(v[i]+k) != hashTable.end()) {
+            count++;
+        }
+    }
+    return count;
+}
+
 int countIntegerPairsWithDifferenceK(vector<int> v, int k) {
-    return countIntegerPairsWithDifferenceK_BruteForce(v, k);
+    //return countIntegerPairsWithDifferenceK_BruteForce(v, k);
+    return countIntegerPairsWithDifferenceK_UsingHashTable(v, k);
 }
 
 TEST_CASE("tests") {
